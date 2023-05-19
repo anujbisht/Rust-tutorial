6f28154eebc5c718dbc5b85caa5faaed41e4e151
root <anujbisht977@gmail.com>
2023-05-19 22:29:17 +0530

    rust programming

--- chap4/Slice_type/target/debug/Slice_type.d
+++
@@ -1,1 +0,0 @@
-/home/kali/rust/chap4/Slice_type/target/debug/Slice_type: /home/kali/rust/chap4/Slice_type/src/main.rs
--- chap4/Slice_type/target/debug/deps/Slice_type-758bcfaf34d8b86a.d
+++
@@ -1,5 +0,0 @@
-/home/kali/rust/chap4/Slice_type/target/debug/deps/Slice_type-758bcfaf34d8b86a: src/main.rs
-
-/home/kali/rust/chap4/Slice_type/target/debug/deps/Slice_type-758bcfaf34d8b86a.d: src/main.rs
-
-src/main.rs:
