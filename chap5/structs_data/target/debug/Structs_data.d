068ef5a227161198275602305c36bd1d59ca6116
root <anujbisht977@gmail.com>
2023-05-19 22:33:43 +0530

    rust programming

--- chap5/structs_data/target/debug/Structs_data.d
+++
@@ -1,1 +0,0 @@
-/home/kali/rust/chap4/structs_data/target/debug/Structs_data: /home/kali/rust/chap4/structs_data/src/main.rs
--- chap5/structs_data/target/debug/deps/Structs_data-91c41c45c9e78a5c.d
+++
@@ -1,5 +0,0 @@
-/home/kali/rust/chap4/Structs_data/target/debug/deps/Structs_data-91c41c45c9e78a5c: src/main.rs
-
-/home/kali/rust/chap4/Structs_data/target/debug/deps/Structs_data-91c41c45c9e78a5c.d: src/main.rs
-
-src/main.rs:
